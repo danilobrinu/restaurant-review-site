8c0dfae0d37e3e33d2e524ee9a5509ac458ac98f
danilobrinu <ddbn.c2@gmail.com>
2019-11-29 01:37:10 +0100

    Add tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  theme: {
+    fontFamily: {
+      body: ['Montserrat', 'sans-serif'],
+    },
+    extend: {
+      inset: {
+        xl: '36rem',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};

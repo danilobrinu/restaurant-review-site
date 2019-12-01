0fe3542ee0582c4c161be7123f475f48582fae97
danilobrinu <ddbn.c2@gmail.com>
2019-12-01 09:58:41 +0100

    Update postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,14 @@
+const tailwindcss = require('tailwindcss');
+const autoprefixer = require('autoprefixer');
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: ['./src/**/*.js', './public/index.html'],
+  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+});
+
+const common = [tailwindcss, autoprefixer];
+const development = [];
+const production = [purgecss];
+
 module.exports = {
-  plugins: [require('tailwindcss'), require('autoprefixer')],
+  plugins: [...[common], ...(process.env.NODE_ENV === 'development' ? development : production)],
 };

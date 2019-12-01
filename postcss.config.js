d0d898970470bbbca18f6dfb80c768867290d190
danilobrinu <ddbn.c2@gmail.com>
2019-12-01 22:58:53 +0100

    Update postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -2,13 +2,16 @@ const tailwindcss = require('tailwindcss');
 const autoprefixer = require('autoprefixer');
 const purgecss = require('@fullhuman/postcss-purgecss')({
   content: ['./src/**/*.js', './public/index.html'],
-  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+});
+const cssnano = require('cssnano')({
+  preset: 'default',
 });
 
 const common = [tailwindcss, autoprefixer];
 const development = [];
-const production = [purgecss];
+const production = [purgecss, cssnano];
 
 module.exports = {
-  plugins: [...[common], ...(process.env.NODE_ENV === 'development' ? development : production)],
+  plugins: [...common, ...(process.env.NODE_ENV === 'development' ? development : production)],
 };

a68bc7652a9cc89071f78179658c1ef6853d1c3e
danilobrinu <ddbn.c2@gmail.com>
2019-12-02 17:45:56 +0100

    Update index file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
 import './styles.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+library.add(fas);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 

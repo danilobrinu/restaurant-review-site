ddfe11aa96c0c653c0b690ec52c2f112bedda384
danilobrinu <ddbn.c2@gmail.com>
2019-12-02 18:30:16 +0100

    Update Review component

--- src/components/Review.js
+++ src/components/Review.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-function Review({ photo, author, date, comment }) {
+function Review({ avatar, author, date, comment }) {
   return (
     <div className="mb-8">
       <div className="flex items-center">
         <div className="w-12 h-12">
-          <img className="w-12 h-12 rounded-full" alt={author} src={photo} />
+          <img className="w-12 h-12 rounded-full" alt={author} src={avatar} />
         </div>
         <div className="ml-2">
           <div className="text-sm leading-snug font-bold text-gray-900">{author}</div>

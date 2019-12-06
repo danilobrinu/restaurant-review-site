067b62322e2eab494a3d3f81a5ac8f3caf45438d
danilobrinu <ddbn.c2@gmail.com>
2019-12-06 18:34:17 +0100

    Update Badge component

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Badge({ className, children, ...props }) {
   return (
     <span
-      className={`inline-block tracking-wide font-semibold px-2 rounded-lg ${className}`}
+      className={`inline-block text-xs capitalize tracking-wide font-semibold px-2 rounded-lg ${className}`}
       {...props}
     >
       {children}

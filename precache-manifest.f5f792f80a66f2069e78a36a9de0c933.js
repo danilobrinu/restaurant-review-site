6635c784f77ddc847ee653d0d5f0fe98a3d8df32
danilobrinu <ddbn.c2@gmail.com>
2019-12-06 13:16:54 +0100

    Updates

---
+++ precache-manifest.f5f792f80a66f2069e78a36a9de0c933.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10ced01b9a494a8698a49040178e828e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63c5b239b7a6d104dc9d",
+    "url": "/static/css/main.15ff179a.chunk.css"
+  },
+  {
+    "revision": "ccab70b89f2f5bd01ce8",
+    "url": "/static/js/2.b8e3ba03.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.b8e3ba03.chunk.js.LICENSE"
+  },
+  {
+    "revision": "63c5b239b7a6d104dc9d",
+    "url": "/static/js/main.d3b35576.chunk.js"
+  },
+  {
+    "revision": "0e8fe6f2498c279d032c",
+    "url": "/static/js/runtime-main.f04d4160.js"
+  }
+]);

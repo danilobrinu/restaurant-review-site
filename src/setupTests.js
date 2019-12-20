b26d03833e610942104afa7a92f19fe768e27655
danilobrinu <ddbn.c2@gmail.com>
2019-12-20 22:48:11 +0100

    Update setup

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,7 +3,13 @@ window.google = {
     LatLng: jest.fn().mockImplementation((lat, lng) => ({ lat, lng })),
     places: {
       PlacesServiceStatus: {
+        INVALID_REQUEST: 'INVALID_REQUEST',
         OK: 'OK',
+        NOT_FOUND: 'NOT_FOUND',
+        OVER_QUERY_LIMIT: 'OVER_QUERY_LIMIT',
+        REQUEST_DENIED: 'REQUEST_DENIED',
+        UNKNOWN_ERROR: 'UNKNOWN_ERROR',
+        ZERO_RESULTS: '',
       },
     },
   },
